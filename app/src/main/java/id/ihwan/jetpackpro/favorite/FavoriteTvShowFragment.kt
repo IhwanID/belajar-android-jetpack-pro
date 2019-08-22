c8aad9e250e723c11419ed16816fafbbefee4e30
Ihwan <ihwan@indosystem.com>
2019-08-22 10:18:51 +0700

    update favorite tv show

--- app/src/main/java/id/ihwan/jetpackpro/favorite/FavoriteTvShowFragment.kt
+++ app/src/main/java/id/ihwan/jetpackpro/favorite/FavoriteTvShowFragment.kt
@@ -1,13 +1,23 @@
 package id.ihwan.jetpackpro.favorite
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.LinearLayoutManager
+import  id.ihwan.jetpackpro.detail.DetailActivity
 import id.ihwan.jetpackpro.R
+import id.ihwan.jetpackpro.data.source.remote.network.response.ResultsData
+import id.ihwan.jetpackpro.databinding.FragmentFavoriteTvShowBinding
+import id.ihwan.jetpackpro.home.movies.adapter.MoviesPagedListAdapter
+import id.ihwan.jetpackpro.utils.Injection
 
 class FavoriteTvShowFragment : Fragment() {
 
@@ -17,13 +27,43 @@ class FavoriteTvShowFragment : Fragment() {
         }
     }
 
+    private val adapterList: MoviesPagedListAdapter by lazy {
+        MoviesPagedListAdapter{ goToDetailMovies(it) }
+    }
+
+    private lateinit var viewModel: FavoriteViewModel
+
+
+    lateinit var binding: FragmentFavoriteTvShowBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_favorite_tv_show, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorite_tv_show, container, false)
+        binding.lifecycleOwner = this@FavoriteTvShowFragment
+
+        viewModel = ViewModelProviders.of(this, Injection.provideViewModelFactory(requireContext()))
+            .get(FavoriteViewModel::class.java)
+
+        binding.favMoviesRV.apply {
+            adapter = adapterList
+            layoutManager = LinearLayoutManager(activity)
+            setHasFixedSize(true)
+        }
+
+        viewModel.favTvShow.observe(this, Observer<PagedList<ResultsData>> {
+            adapterList.submitList(it)
+        })
+
+        return binding.root
     }
 
+    private fun goToDetailMovies(movies: ResultsData){
+        val i = Intent(activity, DetailActivity::class.java)
+        i.putExtra("id", movies)
+        startActivity(i)
+    }
 
 }
